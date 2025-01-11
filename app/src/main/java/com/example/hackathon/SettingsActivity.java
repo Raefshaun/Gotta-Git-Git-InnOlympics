8647244201befc1f920bf50550524164266a3039
Justine Chua <justinechua77@gmail.com>
2025-01-12 05:24:27 +0800

    bug fixes

--- app/src/main/java/com/example/hackathon/SettingsActivity.java
+++ app/src/main/java/com/example/hackathon/SettingsActivity.java
@@ -10,6 +10,9 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -17,6 +20,11 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
 
         // Initialize views
         ImageView imageView = findViewById(R.id.settingsback); // Back button
